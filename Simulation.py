510555ea881ec4110e974aa7857993debd3601be
Andy Miranda <amiranda267@mycr.redwoods.edu>
2025-05-03 23:38:02 -0700

    Almost done

--- Simulation.py
+++ Simulation.py
@@ -1,4 +1,6 @@
 import turtle
+turtle.speed(0)
+turtle.tracer(0)
 from Planet import Planet
 from Solar_system import SolarSystem
 from Sun import Sun
@@ -11,12 +13,12 @@ class Simulation:
         self.height = height
         self.num_periods = num_periods
         self.t = turtle.Turtle()
-        self.t.speed(1000000000000000)
         self.t.hideturtle()
         self.screen = turtle.Screen()
         self.screen.setup(width = self.width, height = self.height)
         self.screen.bgcolor("black")
         self.t.clear()
+        self.t.speed(0)
 
     def run(self):
         self.solar_system.show_planets()
@@ -31,21 +33,25 @@ class Simulation:
 if __name__ == '__main__':
         solar_system = SolarSystem()
         the_sun = Sun("SOL", 5000, 1000000000, 5800, 0, 0)
-        solar_system.add_sun(the_sun)
-
-        earth = Planet("EARTH", 40.5, 100, 75, 0, 75, 20, 0,  "green")
-        solar_system.add_planet(earth)
-
-        mars = Planet("MARS", 40.5, 100, 75, 0, 125, 20, 0,  "red")
-        solar_system.add_planet(mars)
+        earth = Planet("EARTH", 40.5, 100, 75, 0, 85, 30, 0,  "green")
+        mars = Planet("MARS", 20, 100, 75, 0, 115, 25, 0,  "red")
+        venus = Planet("Venus", 40, 100, 75,0,162, 20,0,"orange" )
+        mercury = Planet("Mecury", 20, 75,75,0,202, 20,0, "purple")
 
 
         simulation = Simulation(solar_system, 500, 500, 200000)
-        simulation.run()
+
+
+
 
         solar_system.add_sun(the_sun)
         solar_system.add_planet(earth)
         solar_system.add_planet(mars)
+        solar_system.add_planet(venus)
+        solar_system.add_planet(mercury)
+
+        turtle.update()
+
 
 
         simulation.run()
